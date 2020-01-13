678416fe161d8d4701c5b9b6ec3fb6d720d7a740
Fernando Rama <fernandorama.dev@gmail.com>
2020-01-13 02:54:39 -0300

    Create serverless blur function

--- functions/blur.js
+++ functions/blur.js
@@ -1,9 +1,24 @@
-const { log: print } = console;
+const jimp = require('jimp');
 
-exports.handler = async (...args) => {
-  print(args);
+const imageFormat = jimp.MIME_PNG;
+
+// const { log: print } = console;
+
+const isUrl = (url) => /^https?:\/{2}/.test(url);
+
+exports.handler = async function blur(event) {
+  const imageUrl = event.queryStringParameters.image;
+  if (!isUrl(imageUrl)) {
+    return {
+      statusCode: 400,
+      body: 'Invalid url format',
+    };
+  }
+  const image = await jimp.read(imageUrl);
+  image.blur(5);
+  const imageAsBase64 = await image.getBase64Async(imageFormat);
   return {
     statusCode: 200,
-    body: 'app running',
+    body: imageAsBase64,
   };
 };
